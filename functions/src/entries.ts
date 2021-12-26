09d59ce0c57ec9efa0d57306195a06db8df948ae
Molly White <molly.white5@gmail.com>
2021-12-26 15:50:04 -0500

    + ability to move entries once created

--- functions/src/entries.ts
+++ functions/src/entries.ts
@@ -56,3 +56,20 @@ export const getEntries = functions.https.onCall(async (data?: EntryQuery) => {
   }
   return { entries, hasMore };
 });
+
+export const moveEntry = functions.https.onRequest(async (req, res) => {
+  const collection = await firestore.collection("entries");
+  const documentRef = collection.doc(req.body.currentId);
+  const document = (await documentRef.get()).data();
+  if (document) {
+    const newDocumentRef = await collection.doc(req.body.newId);
+    if (!(await newDocumentRef.get()).exists) {
+      newDocumentRef.set(document);
+      await documentRef.delete();
+      res.status(200).end();
+    } else {
+      res.status(400).send(`Document already exists at ${req.body.newId}`);
+    }
+  }
+  res.status(400).send(`Document not located at ${req.body.currentId}`);
+});
