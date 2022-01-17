c6b28158f1602325794dd033b5bddee6e9589913
Molly White <molly.white5@gmail.com>
2022-01-17 14:24:41 -0500

    Rewrite admin fns

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,3 @@
-import { addAttribution } from "./attribution";
-import { uploadEntry } from "./admin";
-
 import { updateRssOnChange, serveRss, serveStagedRss } from "./rss";
 import { updateWeb1OnChange, serveWeb1 } from "./web1";
 
@@ -10,7 +7,3 @@ exports.serveStagedRss = serveStagedRss;
 
 exports.updateWeb1OnChange = updateWeb1OnChange;
 exports.serveWeb1 = serveWeb1;
-
-// Admin
-exports.uploadEntry = uploadEntry;
-exports.addAttribution = addAttribution;
--- src/js/db/attribution.js
+++ src/js/db/attribution.js
@@ -2,8 +2,8 @@ import { collection, getDoc, doc } from "firebase/firestore/lite";
 import { db } from "./functions";
 
 export const getAttribution = async () => {
-  const entriesCollection = collection(db, "attribution");
-  const docRef = doc(entriesCollection, "images");
+  const attributionCollection = collection(db, "attribution");
+  const docRef = doc(attributionCollection, "images");
   const docSnapshot = await getDoc(docRef);
   return docSnapshot.data();
 };
