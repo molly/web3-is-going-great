53d96c683cc7e24a211242c044e251c17a89396c
Molly White <molly.white5@gmail.com>
2022-02-23 23:30:53 -0500

    collections

--- src/constants/collections.js
+++ src/constants/collections.js
@@ -1,10 +1,13 @@
 const COLLECTIONS = {
   "baller-ape-club": "the people behind Baller Ape Club",
   bayc: "Bored Ape Yacht Club",
+  binance: "Binance",
   "canada-convoy": "the Canada convoy protest",
   "class-action": "class action lawsuits",
+  coinbase: "Coinbase",
   cream: "C.R.E.A.M. Finance",
   "crypto-com": "Crypto.com",
+  cryptopunks: "CryptoPunks",
   "doodled-dragons": "the people behind Doodled Dragons",
   "gary-vee": "Gary Vaynerchuk",
   "jacked-apes": "Jacked Ape Club",
