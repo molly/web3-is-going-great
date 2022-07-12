4b2891afe0762ab4507bfd1e5c34583daaf3360a
Kevin Miller <kevin@kevee.net>
2022-07-11 19:38:50 -0700

    fix(a11y): Add role to navigation

--- src/components/timeline/Header.js
+++ src/components/timeline/Header.js
@@ -34,7 +34,7 @@ const Header = forwardRef(function Header(
 
   const renderLinks = () => (
     <>
-      <ul>
+      <ul role="navigation" aria-label="Main">
         <li>
           <Link href="/what">
             <a>What is web3?</a>
