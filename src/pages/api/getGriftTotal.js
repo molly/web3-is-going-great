858d72923782a9be876f6fdad0cdd40af9b2c376
Molly White <molly.white5@gmail.com>
2022-07-13 23:18:22 -0400

    rename

--- src/pages/api/getGriftTotal.js
+++
@@ -1,6 +0,0 @@
-import { getMetadata } from "../../db/metadata";
-
-export default async function handler(req, res) {
-  const metadata = await getMetadata();
-  res.status(200).json({ total: metadata.griftTotal });
-}
