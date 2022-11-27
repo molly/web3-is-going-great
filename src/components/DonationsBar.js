28ac38b9fcd7a7c090ed335f53856c0807859c1c
Molly White <molly.white5@gmail.com>
2022-11-26 21:14:26 -0500

    Fix display when expenses > donations + credits

--- src/components/DonationsBar.js
+++ src/components/DonationsBar.js
@@ -2,112 +2,124 @@ import PropTypes from "prop-types";
 import useWindowWidth from "../hooks/useWindowWidth";
 import { formatDollarString } from "../js/utilities";
 
+function DonationLabel({ children }) {
+  const windowWidth = useWindowWidth();
+  if (windowWidth === "sm") {
+    return null;
+  }
+  return <span title={children}>{children}</span>;
+}
+
+DonationLabel.propTypes = {
+  children: PropTypes.string.isRequired,
+};
+
 export default function DonationsBar({
   donations,
   totalExpenses,
   remainingCredits,
   usedCredits,
 }) {
-  const windowWidth = useWindowWidth();
   const expensesAfterCredits = totalExpenses - usedCredits;
-  let remainingDonations = donations - expensesAfterCredits;
-  if (remainingDonations < 0) {
-    remainingDonations = 0;
+  let remainingDonations = Math.max(donations - expensesAfterCredits, 0);
+  const remainingExpenses = Math.max(
+    totalExpenses - usedCredits - donations,
+    0
+  );
+
+  let denominator;
+  if (remainingDonations >= 0 || remainingCredits > 0) {
+    denominator = totalExpenses + remainingDonations + remainingCredits;
+  } else {
+    denominator = totalExpenses;
   }
 
-  if (remainingDonations >= 0 || remainingCredits > 0) {
-    const denominator = totalExpenses + remainingDonations + remainingCredits;
-    const usedCreditsWidth = (usedCredits / denominator) * 100;
-    const paidWidth = (expensesAfterCredits / denominator) * 100;
-    const remainingCreditsWidth = (remainingCredits / denominator) * 100;
-    const remainingDonationsWidth = (remainingDonations / denominator) * 100;
-    return (
-      <div className="donations-bar-wrapper">
-        <div className="text-bar">
-          <div style={{ width: `${usedCreditsWidth + paidWidth}%` }}>
-            {formatDollarString(totalExpenses)}
-          </div>
-          {(remainingCredits !== 0 || remainingDonations !== 0) && (
-            <div
-              style={{
-                width: `${remainingCreditsWidth + remainingDonationsWidth}%`,
-              }}
-            >
-              {formatDollarString(remainingCredits + remainingDonations)}
-            </div>
-          )}
+  const usedCreditsWidth = (usedCredits / denominator) * 100;
+  const paidWidth =
+    (Math.min(expensesAfterCredits, donations) / denominator) * 100;
+  const remainingCreditsWidth = (remainingCredits / denominator) * 100;
+  const remainingDonationsWidth = (remainingDonations / denominator) * 100;
+  const remainingExpensesWidth = (remainingExpenses / denominator) * 100;
+
+  return (
+    <div className="donations-bar-wrapper">
+      <div className="text-bar">
+        <div style={{ width: `${usedCreditsWidth + paidWidth}%` }}>
+          {formatDollarString(totalExpenses)}
         </div>
-        <div className="donations-bar">
+        {(remainingCredits !== 0 || remainingDonations !== 0) && (
           <div
-            className="used-credits"
-            style={{ width: `${usedCreditsWidth}%` }}
+            style={{
+              width: `${remainingCreditsWidth + remainingDonationsWidth}%`,
+            }}
           >
-            {windowWidth !== "sm" ? "Credits" : null}
+            {formatDollarString(remainingCredits + remainingDonations)}
           </div>
-          <div className="paid" style={{ width: `${paidWidth}%` }}>
-            {windowWidth !== "sm" ? "Expenses" : null}
-          </div>
-          {remainingCredits !== 0 && (
-            <div
-              className="extra-credits"
-              style={{ width: `${remainingCreditsWidth}%` }}
-            >
-              {windowWidth !== "sm" ? "Remaining credits" : null}
-            </div>
-          )}
-          {remainingDonations !== 0 && (
-            <div
-              className="extra-donations"
-              style={{ width: `${remainingDonationsWidth}%` }}
-            >
-              {windowWidth !== "sm" ? "Remaining donations" : null}
-            </div>
-          )}
-        </div>
-        <div className="text-bar">
-          <div style={{ width: `${usedCreditsWidth}%` }}>
-            {formatDollarString(usedCredits)}
-          </div>
-          <div style={{ width: `${paidWidth}%` }}>
-            {formatDollarString(expensesAfterCredits)}
-          </div>
-          {remainingCredits !== 0 && (
-            <div style={{ width: `${remainingCreditsWidth}%` }}>
-              {formatDollarString(remainingCredits)}
-            </div>
-          )}
-          {remainingDonations !== 0 && (
-            <div style={{ width: `${remainingDonationsWidth}%` }}>
-              {formatDollarString(remainingDonations)}
-            </div>
-          )}
-        </div>
+        )}
+        {remainingExpenses > 0 && (
+          <div
+            style={{ width: `${remainingExpensesWidth}%` }}
+            className="filler"
+          />
+        )}
       </div>
-    );
-  } else {
-    const paidWidth = (donations / totalExpenses) * 100;
-    const remainderWidth = ((totalExpenses - donations) / totalExpenses) * 100;
-    return (
-      <div className="donations-bar-wrapper">
-        <div className="text-bar">
-          <div style={{ width: `${paidWidth}%` }}>
-            {formatDollarString(donations)}
-          </div>
-          <div style={{ width: `${remainderWidth}%` }}>
-            {formatDollarString(totalExpenses - donations)}
-          </div>
+      <div className="donations-bar">
+        <div className="used-credits" style={{ width: `${usedCreditsWidth}%` }}>
+          <DonationLabel>Expenses covered by credits</DonationLabel>
         </div>
-        <div className="donations-bar">
-          <div className="paid" style={{ width: `${paidWidth}%` }}>
-            {windowWidth !== "sm" ? "Donations" : null}
-          </div>
-          <div className="remainder" style={{ width: `${remainderWidth}%` }}>
-            {windowWidth !== "sm" ? "Remaining expenses" : null}
-          </div>
+        <div className="paid" style={{ width: `${paidWidth}%` }}>
+          <DonationLabel>Expenses covered by donations</DonationLabel>
         </div>
+        {remainingCredits !== 0 && (
+          <div
+            className="extra-credits"
+            style={{ width: `${remainingCreditsWidth}%` }}
+          >
+            <DonationLabel>Remaining credits</DonationLabel>
+          </div>
+        )}
+        {remainingDonations !== 0 && (
+          <div
+            className="extra-donations"
+            style={{ width: `${remainingDonationsWidth}%` }}
+          >
+            <DonationLabel>Remaining donations</DonationLabel>
+          </div>
+        )}
+        {remainingExpenses !== 0 && (
+          <div
+            className="remaining-expenses"
+            style={{ width: `${remainingExpensesWidth}%` }}
+          >
+            <DonationLabel>Remaining expenses</DonationLabel>
+          </div>
+        )}
       </div>
-    );
-  }
+      <div className="text-bar">
+        <div style={{ width: `${usedCreditsWidth}%` }}>
+          {formatDollarString(usedCredits)}
+        </div>
+        <div style={{ width: `${paidWidth}%` }}>
+          {formatDollarString(expensesAfterCredits)}
+        </div>
+        {remainingCredits !== 0 && (
+          <div style={{ width: `${remainingCreditsWidth}%` }}>
+            {formatDollarString(remainingCredits)}
+          </div>
+        )}
+        {remainingDonations !== 0 && (
+          <div style={{ width: `${remainingDonationsWidth}%` }}>
+            {formatDollarString(remainingDonations)}
+          </div>
+        )}
+        {remainingExpenses !== 0 && (
+          <div style={{ width: `${remainingExpensesWidth}%` }}>
+            {formatDollarString(remainingExpenses)}
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 DonationsBar.propTypes = {
