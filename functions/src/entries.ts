f0e9833b6a5a52f4b72b754333324585ba0eeb16
Molly White <molly.white5@gmail.com>
2022-12-03 15:06:44 -0500

    Add transform function to run bulk text updates when needed; used for #604 but keeping the function around because it will be useful

--- functions/src/entries.ts
+++ functions/src/entries.ts
@@ -17,3 +17,39 @@ export const moveEntry = functions.https.onRequest(async (req, res) => {
   }
   res.status(400).send(`Document not located at ${req.body.currentId}`);
 });
+
+export const runTransform = functions.https.onRequest(async (req, res) => {
+  const entriesSnapshot = await firestore.collection("entries").get();
+
+  const entriesPromises = entriesSnapshot.docs.map(async (child) => {
+    const data = child.data();
+    let text = data.body;
+
+    let modifiedFlag = false;
+
+    if (/(?<!\d) ?[—–] ?(?![\d$])/.test(text)) {
+      text = text.replace(/(?<!\d) ?[—–] ?(?![\d$])/g, " — ");
+      modifiedFlag = true;
+    }
+
+    if (/[‘’“”]/.test(text)) {
+      text = text.replace(/[‘’]/g, "'");
+      text = text.replace(/[“”]/g, '"');
+      modifiedFlag = true;
+    }
+
+    if (modifiedFlag) {
+      return child.ref.update({ body: text });
+    }
+    return Promise.resolve();
+  });
+
+  Promise.all(entriesPromises)
+    .then(() => {
+      res.status(200).end();
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).end();
+    });
+});
