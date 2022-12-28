a950f2cefef2f34f0f6bc328310db2792bd8addd
Molly White <molly.white5@gmail.com>
2022-12-28 14:57:02 -0500

    Replace with sharper hamburger menu

--- src/components/navigation/MobileNavigation.js
+++ src/components/navigation/MobileNavigation.js
@@ -25,7 +25,16 @@ export default function MobileNavigation() {
         className="mobile-navigation-toggle"
         onClick={() => setIsOpen(!isOpen)}
       >
-        <i className="fas fa-bars" />
+        <svg
+          viewBox="0 0 100 80"
+          width="25"
+          height="25"
+          className="hamburger-icon"
+        >
+          <rect width="100" height="10"></rect>
+          <rect y="30" width="100" height="10"></rect>
+          <rect y="60" width="100" height="10"></rect>
+        </svg>
       </button>
     );
   } else {
