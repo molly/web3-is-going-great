4abb1978adf5a3ca943365e2eaa97cd3f1e6699c
Molly White <molly.white5@gmail.com>
2022-12-28 17:55:15 -0500

    render no js navbar on other pages also

--- src/components/SimpleHeader.js
+++ src/components/SimpleHeader.js
@@ -1,18 +1,25 @@
 import PropTypes from "prop-types";
 import useWindowWidth from "../hooks/useWindowWidth";
 
+import NoJsNavigation from "./navigation/NoJsNavigation";
 import NavigationBar from "./navigation/NavigationBar";
 import MobileNavigation from "./navigation/MobileNavigation";
 import clsx from "clsx";
+import useIsBrowserRendering from "../hooks/useIsBrowserRendering";
 
 export default function SimpleHeader({ children, className, ...rest }) {
+  const isBrowserRendering = useIsBrowserRendering();
   const windowWidth = useWindowWidth();
 
   return (
     <>
-      {windowWidth !== "xs" && windowWidth !== "sm" && <NavigationBar />}
+      {!isBrowserRendering && <NoJsNavigation />}
+      {isBrowserRendering && windowWidth !== "xs" && windowWidth !== "sm" && (
+        <NavigationBar />
+      )}
       <header className={clsx("page-header", className)} {...rest}>
-        {(windowWidth === "sm" || windowWidth === "xs") && <MobileNavigation />}
+        {((isBrowserRendering && windowWidth === "sm") ||
+          windowWidth === "xs") && <MobileNavigation />}
         <h1>{children}</h1>
       </header>
     </>
