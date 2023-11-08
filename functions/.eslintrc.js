53942e1318bcafce5e1394678987ddb03973e60b
Molly White <molly.white5@gmail.com>
2023-11-08 12:46:21 -0500

    Updates

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,14 +1,28 @@
 module.exports = {
+  root: true,
   env: {
-    browser: true,
-    es2021: true,
+    es6: true,
+    node: true,
   },
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    "plugin:@typescript-eslint/recommended",
+  ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: 12,
+    project: ["tsconfig.json", "tsconfig.dev.json"],
     sourceType: "module",
   },
-  plugins: ["@typescript-eslint"],
-  rules: {},
+  ignorePatterns: [
+    "/lib/**/*", // Ignore built files.
+  ],
+  plugins: ["@typescript-eslint", "import"],
+  rules: {
+    quotes: ["error", "double"],
+    "import/no-unresolved": 0,
+    indent: ["error", 2],
+  },
 };
